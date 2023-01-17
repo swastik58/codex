56e5bbe9168c052e1edf89452372792e4b60f6d3
swastik58 <swastiksupakar19@gmail.com>
2023-01-17 13:15:18 +0530

    server updated

--- vanilla/script.js
+++ vanilla/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     //fetch data from server
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-ew80.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
